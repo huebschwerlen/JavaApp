c67251bf43cc7838ac3fa0741b916398def551d7
Sam Huebschwerlen <huebschwerlen@gmail.com>
2021-07-01 11:26:01 -0500

    did some koans

--- koans/src/beginner/AboutAssertions.java
+++ koans/src/beginner/AboutAssertions.java
@@ -10,46 +10,46 @@ public class AboutAssertions {
     @Koan
     public void assertBooleanTrue() {
         // there are two possibilities, true or false, what would it be here?
-        assertTrue(__);
+        assertTrue(true);
     }
 
     @Koan
     public void assertBooleanFalse() {
-        assertFalse(__);
+        assertFalse(false);
     }
 
     @Koan
     public void assertNullObject() {
         // reference to the object can be null, a magic keyword, null, which means
         // that there is nothing there
-        assertNull(__);
+        assertNull(null);
     }
 
     @Koan
     public void assertNullObjectReference() {
-        Object someObject = __;
+        Object someObject = null;
         assertNull(someObject);
     }
 
     @Koan
     public void assertNotNullObject() {
         // but what when there should not be a null value?
-        assertNotNull(null);
+        assertNotNull(4);
     }
 
     @Koan
     public void assertEqualsUsingExpression() {
-        assertTrue("Hello World!".equals(__));
+        assertTrue("Hello World!".equals("Hello World!"));
     }
 
     @Koan
     public void assertEqualsWithAFewExpressions() {
-        assertEquals("Hello World!", __);
-        assertEquals(1, __);
-        assertEquals(2 + 2, __);
-        assertEquals(2 * 3, __);
-        assertEquals(3 - 8, __);
-        assertEquals(10 / 2, __);
+        assertEquals("Hello World!", "Hello World!");
+        assertEquals(1, 1);
+        assertEquals(2 + 2, 4);
+        assertEquals(2 * 3, 6);
+        assertEquals(3 - 8, -5);
+        assertEquals(10 / 2, 5);
     }
 
     @Koan
